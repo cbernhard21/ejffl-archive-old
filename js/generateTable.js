06a57a31972b326dfe33ab03d9fd82696cc66d92
Christopher Bernhard <58096490+cbernhard21@users.noreply.github.com>
2020-11-29 13:45:19 -0600

    updated more records fix NaN issue

--- js/generateTable.js
+++ js/generateTable.js
@@ -1,11 +1,13 @@
 export const generateTable = (items) => {
     let currentStandingsHtml = items.map(item => {
         const winPercentage = item.wins / (item.wins + item.loses);
+        const test = isNaN(winPercentage) ? '.000' : winPercentage.toFixed(3).slice(1);
+
         return `<div class="team-stats-record grid-4">
                     <p>${item.teamName}</p>
                     <p class="align-right">${item.wins}</p>
                     <p class="align-right">${item.loses}</p>
-                    <p class="align-right">${winPercentage.toFixed(3).slice(1)}</p>
+                    <p class="align-right">${test}</p>
                 </div>`
     }).join('');
     return currentStandingsHtml;
