62df690a793afc5e4a9be9d24c6dbbb6d0c81605
Christopher <chris_bernhard@yahoo.com>
2020-12-09 17:13:57 -0600

    added champions and changed nav to js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,11 @@ import { getRegularStandings } from "./getRegularStandings.js"
 import { getPlayoffStandings } from "./getPlayoffStandings.js"
 import { displayMainSummary } from "./displayMainSummary.js"
 import { tradeHistory } from "./tradeHistory.js"
+import { navSlide, navHTML } from "./nav.js"
+import { headerHTML } from "./header.js"
+import { champsHTML } from './champsHTML.js'
 
 const currentPage = window.location.pathname;
-
-
 
 if (currentPage === '/index.html') {
     displayMainSummary();
@@ -15,6 +16,13 @@ if (currentPage === '/index.html') {
     getPlayoffStandings();
 } else if (currentPage === '/trades.html') {
     tradeHistory();
+} else if (currentPage === '/champs.html') {
+    champsHTML();
 } else {
     displayMainSummary();
 }
+
+document.querySelector('header').innerHTML = headerHTML();
+
+document.querySelector('nav').innerHTML = navHTML();
+navSlide();
