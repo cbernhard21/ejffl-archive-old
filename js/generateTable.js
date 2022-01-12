afeccca029fdb8650dbbb5e678dd6c6bc1973105
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-01-12 23:58:20 +0000

    made win percentage sortable

--- js/generateTable.js
+++ js/generateTable.js
@@ -1,13 +1,11 @@
 export const generateTable = (items) => {
     let currentStandingsHtml = items.map(item => {
-        const winPercentage = item.wins / (item.wins + item.loses);
-        const finalWinPercentage = isNaN(winPercentage) ? '.000' : winPercentage.toFixed(3).slice(1);
 
         return `<div class="team-stats-record grid-4">
                     <p>${item.teamName}</p>
                     <p class="align-right">${item.wins}</p>
                     <p class="align-right">${item.loses}</p>
-                    <p class="align-right win-percent">${finalWinPercentage}</p>
+                    <p class="align-right win-percent">${item.winPercentage}</p>
                 </div>`
     }).join('');
     return currentStandingsHtml;
@@ -15,14 +13,14 @@ export const generateTable = (items) => {
 
 export const generatePlayoffTable = (items) => {
     let currentStandingsHtml = items.map(item => {
-        const winPercentage = item.playoffWins / (item.playoffWins + item.playoffLoses);
-        let finalWinPercentage = isNaN(winPercentage) ? '.000' : winPercentage.toFixed(3).slice(1);
+        // const winPercentage = item.playoffWins / (item.playoffWins + item.playoffLoses);
+        // let finalWinPercentage = isNaN(winPercentage) ? '.000' : winPercentage.toFixed(3).slice(1);
 
         return `<div class="team-stats-record grid-4">
                     <p>${item.teamName}</p>
                     <p class="align-right">${item.playoffWins}</p>
                     <p class="align-right">${item.playoffLoses}</p>
-                    <p class="align-right">${finalWinPercentage}</p>
+                    <p class="align-right">${item.playoffWinPercentage}</p>
                 </div>`
     }).join('');
     return currentStandingsHtml;
--- js/getRegularStandings.js
+++ js/getRegularStandings.js
@@ -1,28 +1,40 @@
 import { generateTable } from "./generateTable.js";
 
 export async function getRegularStandings() {
-    const currentStandingsContainer = document.querySelector('#current-standings-container');
-    const pastStandingsContainer = document.querySelector('#past-standings-container');
-    const main = document.querySelector('#main');
-    const response = await fetch('archive.json');
-    const data = await response.json();
-
-    // remove hidden class when finished loading
-    main.classList.remove('is-hidden');
-
-    //sections from the json file to work with
-    const currentOwners = data.current.currentOwners;
-    const pastOwners = data.past.pastOwners;
-
-    //columns to sort by
-    const winsColumnCurrent = document.querySelector('#wins-column-current');
-    const losesColumnCurrent = document.querySelector('#loses-column-current');
-    const percentColumnCurrent = document.querySelector('#percent-column-current');
-    const winsColumnPast = document.querySelector('#wins-column-past');
-    const losesColumnPast = document.querySelector('#loses-column-past');
-    const percentColumnPast = document.querySelector('#percent-column-past');
-
     try {
+        
+        const currentStandingsContainer = document.querySelector('#current-standings-container');
+        const pastStandingsContainer = document.querySelector('#past-standings-container');
+        const main = document.querySelector('#main');
+        const response = await fetch('archive.json');
+        const data = await response.json();
+
+        // remove hidden class when finished loading
+        main.classList.remove('is-hidden');
+
+        //sections from the json file to work with
+        const currentOwners = data.current.currentOwners;
+        const pastOwners = data.past.pastOwners;
+
+        currentOwners.forEach(owner => {
+            let winPercentage = owner.wins / (owner.wins + owner.loses);
+            let finalWinPercentage = isNaN(winPercentage) ? '.000' : winPercentage.toFixed(3).slice(1);
+            owner.winPercentage = finalWinPercentage;
+        })
+
+        pastOwners.forEach(owner => {
+            let winPercentage = owner.wins / (owner.wins + owner.loses);
+            let finalWinPercentage = isNaN(winPercentage) ? '.000' : winPercentage.toFixed(3).slice(1);
+            owner.winPercentage = finalWinPercentage;
+        })
+
+        //columns to sort by
+        const winsColumnCurrent = document.querySelector('#wins-column-current');
+        const losesColumnCurrent = document.querySelector('#loses-column-current');
+        const percentColumnCurrent = document.querySelector('#percent-column-current');
+        const winsColumnPast = document.querySelector('#wins-column-past');
+        const losesColumnPast = document.querySelector('#loses-column-past');
+        const percentColumnPast = document.querySelector('#percent-column-past');
 
         //sort list of owners by wins on page load
         currentOwners.sort((a, b) => b.wins - a.wins);
@@ -41,36 +53,29 @@ export async function getRegularStandings() {
         //sort by loses if loses column is clicked on current teams
         losesColumnCurrent.addEventListener('click', () => {
             currentOwners.sort((a, b) => b.loses - a.loses);
-            pastOwners.sort((a, b) => b.loses - a.loses);
             currentStandingsContainer.innerHTML = generateTable(currentOwners);
-
         });
 
         percentColumnCurrent.addEventListener('click', () => {
-            const winPercent = document.querySelectorAll('.win-percent');
-            for (let i = 0; i < winPercent.length; i++) {
-
-            }
+            currentOwners.sort((a, b) => b.winPercentage - a.winPercentage);
+            currentStandingsContainer.innerHTML = generateTable(currentOwners);
         })
 
         //sort by wins if win column is clicked on past teams
         winsColumnPast.addEventListener('click', () => {
-            currentOwners.sort((a, b) => b.wins - a.wins);
             pastOwners.sort((a, b) => b.wins - a.wins);
-            // currentStandingsContainer.innerHTML = generateTable(currentOwners);
             pastStandingsContainer.innerHTML = generateTable(pastOwners);
         });
 
         //sort by loses if loses column is clicked on past teams
         losesColumnPast.addEventListener('click', () => {
-            currentOwners.sort((a, b) => b.loses - a.loses);
             pastOwners.sort((a, b) => b.loses - a.loses);
-            // currentStandingsContainer.innerHTML = generateTable(currentOwners);
             pastStandingsContainer.innerHTML = generateTable(pastOwners);
         });
 
         percentColumnPast.addEventListener('click', () => {
-            console.log('past percent column was clicked')
+            pastOwners.sort((a, b) => b.winPercentage - a.winPercentage);
+            pastStandingsContainer.innerHTML = generateTable(pastOwners);
         })
 
     } catch (error) {
